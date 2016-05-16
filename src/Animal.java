4d28be5eee3cc625c223c84eab42e0cff958fd92
jonathangunnells <jonathan.gunnells@yahoo.com>
2016-05-16 12:47:17 -0400

    Created 5 examples of classes

--- src/Animal.java
+++ src/Animal.java
@@ -2,8 +2,42 @@
  * Created by jonathangunnells on 5/16/16.
  */
 public class Animal {
-    String name;
-    String species;
-    double weight;
+    String name = "Kevin";
+    String species = "Turtle";
+    double weight = 7.5;
+    boolean isAlive = true;
 
+}
+
+public class Car {
+    String name = "Christine";
+    String model = "Fury";
+    double weight = 4072;
+    boolean killsPeople = true;
+
+
+}
+
+public class Person {
+    String name = "Michael";
+    String job = "RegionalManager";
+    int age = 45;
+    boolean makesSales = false;
+
+}
+
+public class Person {
+    String name = "Dwight";
+    String job = "AssistantToTheRegionalManager";
+    boolean wearsGlasses = true;
+    boolean makesSales = true;
+
+
+}
+
+public class Food{
+    String name = "Burrito";
+    String filling = "Tofu";
+    boolean tastesGood = true;
+    int ingrediants = 43;
 }
